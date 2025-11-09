2688efbc985c32c4dd12059094fe160a46cb5b80
slavikator <slavikator@gmail.com>
2025-11-09 10:10:28 +0200

    lualine config

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -39,12 +39,71 @@ return {
         },
       },
       sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { "filename" },
+        lualine_a = {
+          { 'mode' },
+          {
+            'lsp_status',
+            icon = '', -- f013
+            symbols = {
+              -- Standard unicode symbols to cycle through for LSP progress:
+              spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' },
+              -- Standard unicode symbol for when LSP is done:
+              done = '✓',
+              -- Delimiter inserted between LSP names:
+              separator = ' ',
+            },
+             -- List of LSP names to ignore (e.g., `null-ls`):
+            ignore_lsp = {},
+            -- Display the LSP name
+            show_name = true,
+          },
+          {
+            'diagnostics',
+            sources = { 'nvim_diagnostic', 'coc' },
+            sections = { 'error', 'warn', 'info', 'hint' },
+            diagnostics_color = {
+              error = 'DiagnosticError', -- Changes diagnostics' error color.
+              warn  = 'DiagnosticWarn',  -- Changes diagnostics' warn color.
+              info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
+              hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
+            },
+            symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'},
+            colored = true,           -- Displays diagnostics status in color if set to true.
+            update_in_insert = false, -- Update diagnostics in insert mode.
+            always_visible = false,   -- Show diagnostics even if there are none.
+          }
+        },
+        lualine_b = { "branch", "diff" },
+        lualine_c = {
+          {
+            'filename',
+            file_status = true,      -- Displays file status (readonly status, modified status)
+            newfile_status = false,  -- Display new file status (new file means no write after created)
+            path = 1,                -- 0: Just the filename
+                                     -- 1: Relative path
+                                     -- 2: Absolute path
+                                     -- 3: Absolute path, with tilde as the home directory
+                                     -- 4: Filename and parent dir, with tilde as the home directory
+
+            shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+                               -- for other components. (terrible name, any suggestions?)
+            symbols = {
+              modified = '[+]',      -- Text to show when the file is modified.
+              readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+              unnamed = '[No Name]', -- Text to show for unnamed buffers.
+              newfile = '[New]',     -- Text to show for newly created file before first write
+            }
+          }
+        },
         lualine_x = { "encoding", "fileformat", "filetype" },
         lualine_y = { "progress" },
-        lualine_z = { "location" },
+        lualine_z = {
+          "location",
+          {
+            'datetime',
+            style = '%H:%M',
+          },
+        },
       },
       inactive_sections = {
         lualine_a = {},
@@ -55,12 +114,25 @@ return {
         lualine_z = {},
       },
       tabline = {
-        lualine_c = {
-	  {	
-	    "buffers",
-	    mode = 2,
-          }  
+        lualine_a = {
+          {
+            "buffers",
+            show_filename_only = true,   -- Shows shortened relative path when set to false.
+            hide_filename_extension = false,   -- Hide filename extension when set to true.
+            show_modified_status = true, -- Shows indicator when the buffer is modified.
+            mode = 2, -- 0: Shows buffer name
+                -- 1: Shows buffer index
+                -- 2: Shows buffer name + buffer index
+                -- 3: Shows buffer number
+                -- 4: Shows buffer name + buffer number
+            symbols = {
+              modified = ' ●',      -- Text to show when the buffer is modified
+              alternate_file = '#', -- Text to show to identify the alternate file
+              directory =  '',     -- Text to show when the buffer is a directory
+            },
+          },
         },
+        lualine_c = {},
         lualine_z = { "tabs" },
       },
       winbar = {},
