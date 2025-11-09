b1ff9ebc0c9ad40248d2119ae13f774be574b412
slavikator <slavikator@gmail.com>
2025-11-09 13:45:04 +0200

    Added shotcuts for Copy relative/absolute file pathes

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,21 @@
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", { desc = "Next buffer" })
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { desc = "Previous buffer" })
 
+-- Copy relative file path
+vim.keymap.set("n", "<leader>cp", function()
+  local path = vim.fn.expand("%:.")
+  vim.fn.setreg("+", path)
+  print("📋 Copied relative path: " .. path)
+end, { desc = "Copy relative file path" })
+
+-- Copy absolute file path
+vim.keymap.set("n", "<leader>cP", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  print("📋 Copied absolute path: " .. path)
+end, { desc = "Copy absolute file path" })
+
+
 -- Jump to buffer by number (1–9)
 for i = 1, 9 do
   vim.keymap.set("n", string.format("<leader>%d", i), function()
@@ -17,3 +32,5 @@ for i = 1, 9 do
     end
   end, { desc = "Go to buffer " .. i })
 end
+
+
