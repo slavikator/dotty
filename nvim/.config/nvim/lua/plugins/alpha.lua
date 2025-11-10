2a0d71674d07f52072880376f695135887385871
slavikator <slavikator@gmail.com>
2025-11-10 21:09:34 +0200

    telescope tweaks

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -2,9 +2,9 @@
 return {
   'goolord/alpha-nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' }, -- Optional: for icons
-  config = function ()
-    local alpha = require'alpha'
-    local dashboard = require'alpha.themes.dashboard'
+  config = function()
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.dashboard'
 
     -- Header
     dashboard.section.header.val = {
@@ -21,12 +21,17 @@ return {
     dashboard.section.buttons.val = {
       dashboard.button('n', '  New file', ':enew<CR>'),
       dashboard.button('f', '  Find file', '<cmd>Telescope find_files<CR>'),
+      dashboard.button('b', '  File Browser', ":lua require('telescope').extensions.file_browser.file_browser({ cwd = vim.fn.expand('~') })<CR>"),
       dashboard.button('r', '  Recent files', '<cmd>Telescope oldfiles<CR>'),
-      dashboard.button('c', '  Config', ":lua require('telescope.builtin').find_files({ cwd = vim.fn.stdpath('config'), hidden = true })<CR>"),
       dashboard.button('g', '  Grep text', '<cmd>Telescope live_grep<CR>'),
+      dashboard.button(
+        'c',
+        '  Config',
+        ':lua require(\'telescope.builtin\').find_files({ cwd = vim.fn.stdpath(\'config\'), hidden = true })<CR>'
+      ),
       dashboard.button('l', '  Lazy', ':Lazy<CR>'),
       dashboard.button('m', '  Mason', ':Mason<CR>'),
-      dashboard.button('q', '  Quit', ':q<CR>')
+      dashboard.button('q', '  Quit', ':q<CR>'),
     }
     dashboard.section.buttons.opts.hl = 'Function'
 
@@ -34,7 +39,6 @@ return {
     dashboard.section.footer.val = 'dotty config'
     dashboard.section.footer.opts.hl = 'Comment'
 
-
     alpha.setup(dashboard.opts)
-  end
+  end,
 }
