550cef7f71fc04f40a26d559589549041f8dd3da
slavikator <slavikator@gmail.com>
2025-11-10 13:22:35 +0200

    some tweaks

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,41 +1,57 @@
--- lua/plugins/neo-tree.lua
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
   },
   config = function()
-    -- Unless you are still migrating, remove the deprecated commands
-    vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+    vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
 
-    require("neo-tree").setup({
-      close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
-      popup_border_style = "rounded",
+    require('neo-tree').setup({
+      close_if_last_window = true,
+      popup_border_style = 'rounded',
       enable_git_status = true,
       enable_diagnostics = true,
+
       window = {
-        position = "left",
-        width = 30,
+        position = 'left',
+        width = 40,
         mappings = {
-          ["a"] = "add", -- "add" will prompt for a new file or directory
-          ["<leader>e"] = "close_window",
+          ['a'] = 'add', -- create a new file or directory
         },
       },
+
       filesystem = {
         filtered_items = {
-          visible = true, -- This will show dotfiles in the tree
+          visible = true,
           hide_dotfiles = false,
           hide_gitignored = true,
         },
-        follow_current_file = true, -- Focus the file in the tree when changing buffers.
-        group_empty_dirs = true, -- Group empty directories when expanding.
+        follow_current_file = true,
+        group_empty_dirs = true,
+      },
+
+      event_handlers = {
+        -- Close Neo-tree after opening a file
+        {
+          event = 'file_opened',
+          handler = function() require('neo-tree.command').execute({ action = 'close' }) end,
+        },
+        -- Open newly created file in the main window and close Neo-tree
+        {
+          event = 'file_added',
+          handler = function(file_path)
+            vim.defer_fn(function()
+              vim.cmd('edit ' .. vim.fn.fnameescape(file_path))
+              require('neo-tree.command').execute({ action = 'close' })
+            end, 10)
+          end,
+        },
       },
     })
 
-    -- Keymap to toggle neo-tree
     vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<cr>', { desc = 'Toggle file explorer' })
   end,
 }
