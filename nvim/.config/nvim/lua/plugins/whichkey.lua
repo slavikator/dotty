1f8b514f95eee0d1a796c01fe190d87528557604
slavikator <slavikator@gmail.com>
2025-11-21 20:33:54 +0200

    Which-key custom menu

---
+++ nvim/.config/nvim/lua/plugins/whichkey.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    spec = {
+      { "<leader>z", group = "+custom", icon = "⚡" },
+      { "<leader>zc", group = "+copy", icon = " "},
+    },
+  },
+}
