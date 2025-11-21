7c736d0ab6ac9c10b6a8f7704ea43326f7f5bc94
slavikator <slavikator@gmail.com>
2025-11-21 19:29:37 +0200

    feat: Add various nvim improvements
    This commit introduces a number of quality-of-life improvements to the
    Neovim configuration:
    
    - **autocmds**:
      - Clear search highlight when entering insert mode.
      - Automatically enable line numbers and relative line numbers for all
        buffers except the dashboard.
    
    - **keymaps**:
      - / in visual mode to move lines up/down.
      -  to restart the LSP server.
      -  to make the current file executable.
      -  in normal mode to clear search highlights.
      -  to copy the relative file path.
      -  to copy the absolute file path.
      -  to jump to buffers 1-9.

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -26,3 +26,20 @@ vim.api.nvim_create_autocmd("FileType", {
     })
   end,
 })
+
+-- Clear search highlight when entering insert mode
+vim.api.nvim_create_autocmd('InsertEnter', {
+  callback = function()
+    if vim.v.hlsearch == 1 then vim.cmd 'nohlsearch' end
+  end,
+})
+
+-- Automatic enabling of numbering and statuscolumn for real documents
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  callback = function()
+    if vim.bo.filetype ~= 'dashboard' then
+      vim.wo.number = true
+      vim.wo.relativenumber = true
+    end
+  end,
+})
--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -8,6 +8,50 @@ vim.keymap.set("n", "<leader>a", "ggVG", { desc = "Select whole file" })
 -- Copy whole file to system clipboard
 vim.keymap.set("n", "<leader>A", ":%y+<CR>", { desc = "Yank whole file to clipboard" })
 
+-- Move selected lines up/down in visual mode
+vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv', {desc = 'Move selected lines up'})
+vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv', {desc = 'Move selected lines down'})
+
+-- Restart LSP server
+vim.keymap.set('n', '<leader>zr', '<cmd>LspRestart<cr>', {desc = 'Restart LSP server'})
+
+-- Make current file executable
+vim.keymap.set('n', '<leader>zx', '<cmd>!chmod +x %<CR>', { silent = true, desc = 'Make current file executable'})
+
+-- Clear search highlight on <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR><Esc>', { desc = 'Clear search highlight' })
+
+-- Copy relative file path
+vim.keymap.set('n', '<leader>zcr', function()
+  local path = vim.fn.expand '%:.'
+  vim.fn.setreg('+', path)
+  print('📋 Copied relative path: ' .. path)
+end, { desc = 'Copy relative file path' })
+
+-- Jump to buffer by number (1–9)
+for i = 1, 9 do
+  vim.keymap.set('n', string.format('<leader>%d', i), function()
+    local bufs = vim.api.nvim_list_bufs()
+    local valid_bufs = vim.tbl_filter(
+      function(b) return vim.api.nvim_buf_is_loaded(b) and vim.bo[b].buflisted end,
+      bufs
+    )
+
+    if valid_bufs[i] then
+      vim.api.nvim_set_current_buf(valid_bufs[i])
+    else
+      vim.notify('Buffer ' .. i .. ' does not exist', vim.log.levels.WARN)
+    end
+  end, { desc = 'Go to buffer ' .. i })
+end
+
+-- Copy absolute file path
+vim.keymap.set('n', '<leader>zca', function()
+  local path = vim.fn.expand '%:p'
+  vim.fn.setreg('+', path)
+  print('📋 Copied absolute path: ' .. path)
+end, { desc = 'Copy absolute file path' })
+
 -- Toggle TypeScript extra
 vim.keymap.set("n", "<leader>ut", function()
   local path = vim.fn.stdpath("config") .. "/lua/config/lazy.lua"
