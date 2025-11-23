cc3ebb9ee97dc1fb30509ffc7e956201f3e1462c
slavikator <slavikator@gmail.com>
2025-11-23 13:18:16 +0200

    Disable autoformat for lua

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,4 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.autoformat = false
+-- vim.g.autoformat = false
+
+-- Disable autoformat for lua files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "lua" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
